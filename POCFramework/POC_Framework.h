9b07df4658e87a339b9a318f0a92597f3f4a8811
Jegadeeswaran <jegadeesmkce@gmail.com>
2025-07-24 23:09:00 +0530

    add framework

---
+++ POCFramework/POC_Framework.h
@@ -0,0 +1,18 @@
+//
+//  POC_Framework.h
+//  POC Framework
+//
+//  Created by Jegadeeswaran on 24/07/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for POC_Framework.
+FOUNDATION_EXPORT double POC_FrameworkVersionNumber;
+
+//! Project version string for POC_Framework.
+FOUNDATION_EXPORT const unsigned char POC_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POC_Framework/PublicHeader.h>
+
+
